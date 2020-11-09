f44fa8083349f0b493208608ed097aa61a8960f9
Kayla Beaman <kbeaman@ucsc.edu>
2020-11-09 15:09:33 -0800

    removed unnecessary directory

--- public/client/client-app.js
+++
@@ -1,8 +0,0 @@
-const socket = io();
-
-socket.on('returnRoomReq', function(roomName) {
-	var returnName = getElementById("NameSpace");
-	var nameString = createElement("p");
-	nameString.innerHTML = "The room code is " + roomName;
-	returnName.appendChild(nameString);
-});
